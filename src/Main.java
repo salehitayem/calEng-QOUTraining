a227918a1a9e0533f8d3ea21d5f1a02086d20318
Saleh Itayem <salehktaweel@gmail.com>
2025-11-01 09:19:07 +0200

    add comments :)

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 class Main {
     public static void main(String[] args) {
 
-
+        //check for args
         if (args.length == 0 || args.length != 3) {
             System.err.println("Usage: java Main 2 + 3");
             System.exit(1);
         }
+        //calling a method todo the job :)
         handleCommandLine(args);
 
 
     }
-
+    // calculator method working based on atriputes
     static void calEn(char operation, double firstval , double secval) {
         switch (operation) {
             case '+' -> {System.out.println(firstval + secval); break;}
@@ -20,12 +21,13 @@ class Main {
             default -> System.out.println("Unknown operation, oporations ( + , *, - , / )");
         }
     }
-
+    //handle the inputs
     private static void handleCommandLine(String[] args) {
         try{
             double firstval =Double.parseDouble(args[0]);
             double secval = Double.parseDouble(args[2]);
             char op = args[1].charAt(0);
+            //calling the calculator method
             calEn(op, firstval, secval);
         }catch (NumberFormatException e){
             System.err.println("Usage: java Main 2 + 3");
